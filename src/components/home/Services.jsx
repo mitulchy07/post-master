f7318371e4e153ebf0694834580553fce2991b2e
Md Alamin <alaminduc516@gmail.com>
2023-04-01 19:27:41 +0600

    Services and Layout components created

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import Layout from "../Layout"
 
-const Services = () => {
+function Services({ services }) {
     return (
-        <div>
-            <h1>Hello</h1>
+      <Layout>
+        <h1 className="text-2xl font-bold mb-4">Our Services</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {services.map(service => (
+            <div key={service.id} className="p-4 border rounded-md">
+              <h2 className="text-lg font-medium mb-2">{service.title}</h2>
+              <p className="text-gray-600">{service.description}</p>
+              <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Learn More
+              </button>
+            </div>
+          ))}
         </div>
-    );
-}
-
-export default Services;
+      </Layout>
+    )
+  }
+  
+  export default Services
